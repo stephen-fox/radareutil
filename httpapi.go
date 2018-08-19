dc05c4b1843334fa97554b7ddcb9f00ec5cea909
Stephen Fox <stephen.j.fox.jr@gmail.com>
2018-08-18 23:26:25 -0400

    Added 'HttpApiOptions' for future use.

--- httpapi.go
+++ httpapi.go
@@ -13,6 +13,10 @@ const (
 	cmdSubPath = "/cmd"
 )
 
+type HttpApiOptions struct {
+	Timeout time.Duration
+}
+
 type HttpApi interface {
 	Exec(command string) (string, error)
 }
@@ -53,10 +57,14 @@ func (o defaultHttpApi) Exec(command string) (string, error) {
 	return content, nil
 }
 
-func NewHttpApi(address *url.URL) (HttpApi, error) {
+func NewHttpApi(address *url.URL, options HttpApiOptions) (HttpApi, error) {
+	if options.Timeout == 0 {
+		options.Timeout = 10 * time.Second
+	}
+
 	return defaultHttpApi{
 		httpClient: &http.Client{
-			Timeout: 10 * time.Second,
+			Timeout: options.Timeout,
 		},
 		address: address,
 	}, nil

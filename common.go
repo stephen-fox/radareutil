a2caa1152241ef5516c7493e18094d1d46e361a5
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-05-12 17:49:53 -0400

    Separate HTTP server / client code from r2 process code.

--- common.go
+++ common.go
@@ -1,6 +1,19 @@
 package radareutil
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os/exec"
+	"path/filepath"
+	"sync"
+)
+
 type State string
+
+func (o State) String() string {
+	return string(o)
+}
 
 const (
 	Stopped State = "stopped"
@@ -23,4 +36,205 @@ func (o *StoppedInfo) Err() error {
 
 func (o *StoppedInfo) CombinedOutput() string {
 	return o.out
+}
+
+type Radare2Config struct {
+	ExecutablePath     string
+	DoNotTrimOutput    bool
+	SaveOutput         bool
+	DebugPid           int
+	RunHttpServer      bool
+	DisableHttpSandbox bool
+	HttpPort           int
+	DetachOnStop       bool
+}
+
+func (o *Radare2Config) Validate() error {
+	exePathFinal, err := fullyQualifiedBinaryPath(o.ExecutablePath)
+	if err != nil {
+		return err
+	}
+
+	o.ExecutablePath = exePathFinal
+
+	return nil
+}
+
+func (o *Radare2Config) Args() []string {
+	var args []string
+
+	if o.RunHttpServer {
+		if o.HttpPort > 0 {
+			args = append(args, fmt.Sprintf("%s%d", httpServerArg, o.HttpPort))
+		} else {
+			args = append(args, httpServerArg)
+		}
+
+		if o.DisableHttpSandbox {
+			args = append(args, "-e", "http.sandbox=false")
+		}
+	}
+
+	if o.DebugPid > 0 {
+		args = append(args, "-d", fmt.Sprintf("%d", o.DebugPid))
+	} else {
+		args = append(args, "--")
+	}
+
+	return args
+}
+
+type r2Proc struct {
+	config  *Radare2Config
+	mutex   *sync.Mutex
+	state   State
+	stopped chan StoppedInfo
+	cmd     *exec.Cmd
+	stdin   io.Writer
+	stdout  io.Reader
+	stderr  io.Reader
+}
+
+func (o *r2Proc) Status() Status {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+
+	return Status{
+		State: o.state,
+	}
+}
+
+func (o *r2Proc) OnStopped() chan StoppedInfo {
+	return o.stopped
+}
+
+func (o *r2Proc) Start() error {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+
+	if o.state == Running {
+		return fmt.Errorf("radare2 process is already running")
+	}
+
+	err := o.config.Validate()
+	if err != nil {
+		return err
+	}
+
+	radare := exec.Command(o.config.ExecutablePath, o.config.Args()...)
+	radare.Dir = filepath.Dir(o.config.ExecutablePath)
+
+	stdin, err := radare.StdinPipe()
+	if err != nil {
+		return fmt.Errorf("failed to get stdin pipe - %s", err.Error())
+	}
+
+	stdout, err := radare.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("failed to get stdout pipe - %s", err.Error())
+	}
+
+	stderr, err := radare.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("failed to get stderr pipe - %s", err.Error())
+	}
+
+	err = radare.Start()
+	if err != nil {
+		return fmt.Errorf("failed to start radare - %s", err.Error())
+	}
+
+	var output *bytes.Buffer
+	if o.config.SaveOutput {
+		output = bytes.NewBuffer(nil)
+		writeMutex := &sync.Mutex{}
+		w := newSyncBuffer(output, writeMutex)
+		o.stderr = io.TeeReader(stderr, w)
+		o.stdout = io.TeeReader(stdout, w)
+	} else {
+		o.stderr = stderr
+		o.stdout = stdout
+	}
+
+	o.state = Running
+	o.cmd = radare
+	o.stdin = stdin
+
+	go o.monitor(output)
+
+	return nil
+}
+
+func (o *r2Proc) monitor(output *bytes.Buffer) {
+	err := o.cmd.Wait()
+
+	o.mutex.Lock()
+
+	var info StoppedInfo
+
+	if output != nil {
+		info.out = output.String()
+	}
+
+	if o.state != Stopped {
+		o.state = Dead
+		info.err = err
+	}
+
+	select {
+	case o.stopped <- info:
+	default:
+	}
+
+	o.cmd = nil
+
+	o.mutex.Unlock()
+}
+
+func (o *r2Proc) Kill() {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+
+	if o.state != Running {
+		return
+	}
+
+	o.state = Stopped
+
+	o.cmd.Process.Kill()
+}
+
+type syncBuffer struct {
+	mutex *sync.Mutex
+	buff  *bytes.Buffer
+}
+
+func (o *syncBuffer) Write(p []byte) (n int, err error) {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+
+	return o.buff.Write(p)
+}
+
+func (o *syncBuffer) String() string {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+
+	return o.buff.String()
+}
+
+func newSyncBuffer(buffer *bytes.Buffer, mutex *sync.Mutex) *syncBuffer {
+	return &syncBuffer{
+		mutex: mutex,
+		buff:  buffer,
+	}
+}
+
+func newR2Proc(config *Radare2Config) (*r2Proc, error) {
+	return &r2Proc{
+		config:  config,
+		mutex:   &sync.Mutex{},
+		state:   Stopped,
+		stopped: make(chan StoppedInfo),
+	}, nil
 }
--- httpserver.go
+++ httpserver.go
@@ -2,15 +2,94 @@ package radareutil
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
+	"time"
 )
 
 const (
 	httpServerArg = "-c=h"
 )
+
+type httpServerApi struct {
+	config  *Radare2Config
+	client  *http.Client
+	address *url.URL
+	r2      *r2Proc
+}
+
+func (o *httpServerApi) Start() error {
+	return o.r2.Start()
+}
+
+func (o *httpServerApi) Kill() {
+	o.r2.Kill()
+}
+
+func (o *httpServerApi) Status() Status {
+	return o.r2.Status()
+}
+
+func (o *httpServerApi) OnStopped() chan StoppedInfo {
+	return o.r2.OnStopped()
+}
+
+func (o *httpServerApi) Execute(command string) (string, error) {
+	current := o.r2.Status().State
+	if current != Running {
+		return "", fmt.Errorf("cannot execute command - state is %s", current)
+	}
+
+	result, err := executeHttpCall(command, o.address, o.client)
+	if err != nil {
+		return result, err
+	}
+
+	if !o.config.DoNotTrimOutput {
+		result = strings.TrimSpace(result)
+	}
+
+	return result, nil
+}
+
+func NewHttpServerApi(config *Radare2Config) (Api, error) {
+	a, err := url.Parse(fmt.Sprintf("http://127.0.0.1:%d", config.HttpPort))
+	if err != nil {
+		return nil, err
+	}
+
+	r2, err := newR2Proc(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &httpServerApi{
+		config:  config,
+		r2:      r2,
+		client:  &http.Client{
+			Timeout: 5 * time.Second,
+		},
+		address: a,
+	}, nil
+}
+
+func NewCustomHttpServerApi(address *url.URL, httpClient *http.Client, config *Radare2Config) (Api, error) {
+	r2, err := newR2Proc(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &httpServerApi{
+		config:  config,
+		r2:      r2,
+		client:  httpClient,
+		address: address,
+	}, nil
+}
 
 func executeHttpCall(command string, address *url.URL, httpClient *http.Client) (string, error) {
 	resp, err := httpClient.Get(address.String() + cmdSubPath + "/" + command)

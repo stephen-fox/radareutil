d9eb8cfc80d9e39bf17ac4cdf008deb1a3202614
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-05-12 21:57:38 -0400

    Added original code doc. to new HTTP server functionality.

--- http.go
+++ http.go
@@ -56,6 +56,57 @@ func (o *httpServerApi) Execute(command string) (string, error) {
 	return result, nil
 }
 
+// NewHttpServerApi returns a new instance of radare2 running in HTTP
+// server mode.
+//
+// WARNING: This insecure - use at your own risk!
+//
+// Sadly, there is not really any documentation about this feature. Here is a
+// good primer by Megabeets describing how this works:
+// (https://reverseengineering.stackexchange.com/a/18345)
+//
+//	radare2 comes with its own webserver. Although at first, it might seems
+//	like an overkill, its actually quite useful, especially when you want
+//	to debug embedded systems, or simply to execute commands from a remote
+//	terminal.
+//
+//	Simply launch the web server with =h <port> and connect to it with any
+//	HTTP client.
+//
+//	You can print the help for this command by using =h?:
+//
+//	[0x00000000]> =h?
+//	|Usage:  =[hH] [...] # http server
+//	| http server:
+//	| =h port       listen for http connections (r2 -qc=H /bin/ls)
+//	| =h-           stop background webserver
+//	| =h--          stop foreground webserver
+//	| =h*           restart current webserver
+//	| =h& port      start http server in background
+//	| =H port       launch browser and listen for http
+//	| =H& port      launch browser and listen for http in background
+//
+//	So let's use a oneliner command to spawn a radare2 web server with a
+//	session to our beloved /bin/ls/:
+//
+//	$ r2 -c=h /bin/ls
+//	Starting http server...
+//	open http://localhost:9090/
+//	r2 -C http://localhost:9090/cmd/
+//
+//	Good, now that we have an HTTP server running with an open session,
+//	let's connect to it.
+//
+//	You can do this with curl:
+//
+//	$ curl http://127.0.0.1:9090/cmd/?EHello,World!
+//	.--.     .--------------.
+//	| _|     |              |
+//	| O O   <  Hello,World! |
+//	|  |  |  |              |
+//	|| | /   `--------------'
+//	|`-'|
+//	`---'
 func NewHttpServerApi(config *Radare2Config) (Api, error) {
 	a, err := url.Parse(fmt.Sprintf("http://127.0.0.1:%d", config.HttpPort))
 	if err != nil {
